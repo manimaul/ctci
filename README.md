0192a8a5642d0164a2536a55747262b535fa0d3c
Manimaul <manimaul@gmail.com>
2013-11-06 15:39:09 -0800

    questions 1 through 6

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository is my attempt to study for Coding Interview questions.  
+The solutions found here may not necessarily represent the "best" solution as they represent real life practice using a simple text editory, the command line, a white board and little to no internet help except for if I'm really stuck.  
+
+Practice questions taken from "Cracking The Coding Interview" by Gayle Laakmann McDowell  
+http://crackingthecodinginterview.com/
---
+++ q1_6.java
@@ -0,0 +1,69 @@
+//Given an image represented by an NxN matrix where each pixel in the image is 4 bytes, write a method to rotate the image 90 degrees.  Can you do this in place?
+
+class q1_6 {
+	public static void rotateMatrix(int[][] matrix) {
+		int layers = matrix.length / 2;
+		int size = matrix.length-1;
+		int tmpMe;
+
+		for (int i = 0; i < layers; i++) {
+			//i is the current layer
+			for (int n = 0; n < layers; n++) {
+				//cache me
+				tmpMe = matrix[i][n];
+				
+				if (i==n) {
+					//set me to value of next
+					matrix[i][n] = matrix[size-i][n];
+
+					//set next to the value of opposite
+					matrix[size-i][n] = matrix[size-i][size-n];
+
+					//set opposite to the value of previous
+					matrix[size-i][size-n] = matrix[i][size-n];
+
+					//set previous to the value of me
+					matrix[i][size-n] = tmpMe;
+				} else {
+					//set me to value of next
+					matrix[i][n] = matrix[size-n][i];
+
+					//set next to the value of opposite
+					matrix[size-n][i] = matrix[size-i][size-n];
+
+					//set opposite to the value of previous
+					matrix[size-i][size-n] = matrix[n][size-i];
+
+					//set previous to the value of me
+					matrix[n][size-i] = tmpMe;
+				}
+				
+
+			}
+		}
+
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		System.out.println("=============================");
+		for (int i = 0; i < matrix.length; i++) {
+			for (int n = 0; n < matrix.length; n++) {
+				System.out.print(String.format("%02d  ", matrix[i][n]));
+			}
+			System.out.println();	
+		}
+		System.out.println("=============================");
+	}
+
+	public static void main (String[] args) {
+		int[][] matrix = { { 0,  1,  2,  3,  4,  5  },
+					 	   { 6,  7,  8,  9,  10, 11 },
+					 	   { 12, 13, 14, 15, 16, 17 },  
+					 	   { 18, 19, 20, 21, 22, 23 },
+					 	   { 24, 25, 26, 27, 28, 29 },
+					 	   { 30, 31, 32, 33, 34, 35 }  };
+		printMatrix(matrix);
+		rotateMatrix(matrix);
+		printMatrix(matrix);
+	}
+}
