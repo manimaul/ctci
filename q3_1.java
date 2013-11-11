3bce36ab2991ddcf3e027f2b64741e527620f6a7
Manimaul <manimaul@gmail.com>
2013-11-10 21:11:15 -0800

    q3_1

---
+++ q3_1.java
@@ -0,0 +1,171 @@
+//describe you could use a single array to implement 3 stacks
+
+class q3_1 {
+	public static void main(String[] args) {
+		TripleStackArray tsa = new TripleStackArray(3);
+		try {
+			tsa.push("stack # 3", 3);
+			tsa.push("from ", 3);
+			tsa.push("greetings ", 3);
+
+			tsa.push("o ", 1);
+			tsa.push("ll", 1);
+			tsa.push("he", 1);
+			tsa.push("e, ", 2);
+			tsa.push("er", 2);
+			tsa.push("th", 2);
+
+			System.out.print(tsa.pop(1));
+			System.out.print(tsa.pop(1));
+			System.out.print(tsa.pop(1));
+			System.out.print(tsa.pop(2));
+			System.out.print(tsa.pop(2));
+			System.out.print(tsa.pop(2));
+			System.out.print(tsa.pop(3));
+			System.out.print(tsa.pop(3));
+			System.out.print(tsa.pop(3));
+			System.out.println();
+
+			tsa.push("stack # 3", 3);
+			tsa.push("from ", 3);
+			tsa.push("greetings ", 3);
+
+			tsa.push("o ", 1);
+			tsa.push("ll", 1);
+			tsa.push("he", 1);
+			tsa.push("e, ", 2);
+			tsa.push("er", 2);
+			tsa.push("th", 2);
+
+			System.out.print(tsa.peek(1));
+			System.out.print(tsa.peek(2));
+			System.out.print(tsa.peek(3));
+
+			System.out.print(tsa.pop(1));
+			System.out.print(tsa.pop(1));
+			System.out.print(tsa.pop(1));
+			System.out.print(tsa.pop(2));
+			System.out.print(tsa.pop(2));
+			System.out.print(tsa.pop(2));
+			System.out.print(tsa.pop(3));
+			System.out.print(tsa.pop(3));
+			System.out.print(tsa.pop(3));
+			System.out.println();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+//answer
+//Create an array of fixed size * 3, track section size.
+//Write methods to retrieve stack index in array based on tracked size
+//Write methods to track size of 3 stacks
+class TripleStackArray {
+
+	final Object[] mArray;
+	int ssz1 = 0; //stack 1 size
+	int ssz2 = 0;
+	int ssz3 = 0;
+
+	public TripleStackArray(int stackSize) {
+		mArray = new Object[stackSize * 3];
+	}
+	
+	private int getIndexForStack(int pStackNumber) throws Exception {
+		int index;
+		
+		switch (pStackNumber) {
+			case 1:
+			index = ssz1 - 1;
+			break;
+			
+			case 2:
+			index = mArray.length / 3 + ssz2 - 1;
+			break;
+
+			case 3:
+			index = mArray.length / 3 * 2 + ssz3 - 1;
+			break;
+
+			default:
+			throw new Exception("no such stack number");
+		} 
+
+		return index;
+	}
+
+	private void setSizeForStack(int pStackNumber, int size) throws Exception {
+		int maxSize = mArray.length / 3;
+
+		if (size > maxSize)
+			throw new Exception("stack size limit reached");
+		else if(size < 0) {
+			throw new Exception("stack size cannot be less than 0");
+		}
+
+		switch (pStackNumber) {
+			case 1:
+			ssz1 = size;
+			break;
+
+			case 2:
+			ssz2 = size;
+			break;
+
+			case 3:
+			ssz3 = size;
+			break;
+
+			default:
+			throw new Exception("no such stack number");
+		}
+
+	}
+
+	private int getSizeForStack(int pStackNumber) throws Exception {
+		int size;
+		
+		switch (pStackNumber) {
+			case 1:
+			size = ssz1;
+			break;
+
+			case 2:
+			size = ssz2;
+			break;
+
+			case 3:
+			size = ssz3;
+			break;
+
+			default:
+			throw new Exception("no such stack number");
+
+		}
+		return size;
+	}
+
+	public Object pop(int pStackNumber) throws Exception {
+		int index = getIndexForStack(pStackNumber);
+		int size = getSizeForStack(pStackNumber);
+		setSizeForStack(pStackNumber, size - 1);
+		return mArray[index];
+	}
+
+	public void push(Object pObj, int pStackNumber) throws Exception {
+		int size = getSizeForStack(pStackNumber);
+		setSizeForStack(pStackNumber, size + 1);
+		int index = getIndexForStack(pStackNumber);
+		mArray[index] = pObj;
+
+	}
+
+	public Object peek(int pStackNumber) throws Exception {
+		int index = getIndexForStack(pStackNumber);
+		return mArray[index];
+	}
+ 
+}
